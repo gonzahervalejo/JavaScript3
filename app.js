428f36908124960d01d862627aa722e4f8d96e5b
gonzahervalejo <gonzalohervalejo@hotmail.com>
2022-02-07 02:38:37 -0300

    ciclos

---
+++ app.js
@@ -0,0 +1,35 @@
+//CICLOS CLASE 3
+
+//ejemplo 1
+
+// for (i=0; i<10;i++) {
+//     let usuario = prompt("Ingrese su nombre");
+//     let turno = i;
+//     alert (`Bienvenido ${usuario } Usted tiene el turno N° ${turno}`);
+    
+// }
+
+
+
+//ejemplo2
+
+// let condicion;
+
+// do{
+//     let numero1 = parseInt(prompt("Ingrese un numero para sumar"));
+//     let numero2 = parseInt(prompt("Ingrese otro numero"));
+//     let condicion = prompt("Desea seguir operando?");
+    
+//     console.log(`el resultado es ${numero1 + numero2}`);
+
+// }while(condicion == "si");
+
+
+
+
+
+
+
+
+
+
