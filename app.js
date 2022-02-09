a194bcffce526bb79b31ae54d68db5f127dc1feb
gonzahervalejo <gonzalohervalejo@hotmail.com>
2022-02-09 03:00:58 -0300

    funciones

--- app.js
+++ app.js
@@ -26,6 +26,55 @@
 
 
 
+/* calse 4
+Funciones
+Venta de soquetes */
+
+
+const productos = ()=> {
+
+let soquetesId;
+
+    do {
+       soquetesId = parseInt(
+        prompt("Bienvenido! Que producto desea llevar?\n1)Soquetes de corazon \n2) Soquetes de mariposas \n 3)Soquetes de rosas \n 4) soquetes de arcoiris"))
+
+    }while(soquetesId < 1 || soquetesId > 4 ||  isNaN(soquetesId))
+
+
+let soquetesNombre;
+
+switch(soquetesId){
+
+    case 1:
+        soquetesNombre = "Soquetes de corazon"
+        break;
+
+    case 2:
+        soquetesNombre = "Soquetes de mariposas"
+        break;
+
+    case 3:
+        soquetesNombre = "Soquetes de rosas"
+        break;
+    
+    case 4: 
+        soquetesNombre = "Soquetes de arcoiris"
+        break;
+
+        
+}
+    return soquetesNombre;
+
+}
+
+let soquetesNombre = productos();
+productos();
+console.log(soquetesNombre);
+
+
+/* no entiendo porque no me elige el producto a la primera, tengo que tocarlo dos veces*/ 
+
 
 
 
